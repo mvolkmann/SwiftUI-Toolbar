1622750414eeaf64a39d844ffd6c079eb71b0d59
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-02 16:35:19 -0500

    initial version

--- SwiftUI-Toolbar/ContentView.swift
+++ SwiftUI-Toolbar/ContentView.swift
@@ -1,16 +1,58 @@
-//
-//  ContentView.swift
-//  SwiftUI-Toolbar
-//
-//  Created by R. Mark Volkmann on 11/2/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var selection = "Tap a toolbar button."
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Text(selection).padding()
+                .navigationTitle("Toolbar Demo")
+                .toolbar { // must be inside a NavigationView
+                    // placement attribute values include:
+                    // - .navigationBarLeading: top, left-justified
+                    // - .navigationBarTrailing: top, right-justified
+                    // - .bottomBar: bottom, centered
+                    // - other values seem to not handle multiple buttons
+                    // Can use ToolbarItem instead of ToolbarItemGroup
+                    // if just one button inside.
+                    ToolbarItemGroup(placement: .navigationBarTrailing) {
+                        // If there are only two buttons, they are
+                        // laid out as if there is a Spacer between them.
+                        // For other layouts, add your own Spacer views.
+                        Button(
+                            action: { selection = "planes" },
+                            label: { Image(systemName: "airplane") }
+                        )
+                        Button(
+                            action: { selection = "trains" },
+                            label: { Image(systemName: "tram") }
+                        )
+                        Button(
+                            action: { selection = "automobiles" },
+                            label: { Image(systemName: "car.fill") }
+                        )
+                    }
+                    ToolbarItemGroup(placement: .bottomBar) {
+                        Button(
+                            action: { selection = "hearts" },
+                            label: { Image(systemName: "suit.heart.fill") }
+                        )
+                        Button(
+                            action: { selection = "diamonds" },
+                            label: { Image(systemName: "suit.diamond.fill") }
+                        )
+                        Button(
+                            action: { selection = "clubs" },
+                            label: { Image(systemName: "suit.club.fill") }
+                        )
+                        Button(
+                            action: { selection = "spades" },
+                            label: { Image(systemName: "suit.spade.fill") }
+                        )
+                        Spacer()
+                    }
+                }
+        }
     }
 }
 
