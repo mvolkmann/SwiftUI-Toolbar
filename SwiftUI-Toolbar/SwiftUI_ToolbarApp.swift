ff82a7436dbf9c3b797a421631f27c5d0033a5f1
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-11-02 14:55:41 -0500

    Initial Commit

---
+++ SwiftUI-Toolbar/SwiftUI_ToolbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ToolbarApp.swift
+//  SwiftUI-Toolbar
+//
+//  Created by R. Mark Volkmann on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ToolbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
